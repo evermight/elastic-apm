33a8cde4e92ea77ea6a5928a4af2a2957e5e583f
evermight <support@evermight.com>
2024-01-21 01:54:24 +0000

    span details

--- nodejs/index.js
+++ nodejs/index.js
@@ -14,13 +14,20 @@ const port = 3030
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/path/:params1', (req, res) => {
+  res.send('Path')
+  apm.setTransactionName('/path/'+req.params.param1)
+})
 app.get('/demo', (req, res) => {
   res.send('Welcome Demo')
+  apm.setCustomContext({'apm': {'alpha': 'beta computer'}});
 })
 app.get('/error', (req, res) => {
+  const span = apm.startSpan('This is a label')
   res.send('Capture Error!')
   const err = new Error('Trigger Error!')
   apm.captureError(err)
+  span.end()
 })
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
